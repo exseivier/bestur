1fcf35ae869ee8bc35ea5c9e5be9f9601aa459c6
Exseivier Nope <exseivier@users.noreply.github.com>
2019-01-06 11:40:12 -0600

    Basic module scripts were added
    Sequences object creator and dna procedures and statistics

---
+++ scripts/dnaprocedures.py
@@ -0,0 +1,8 @@
+########################################################################
+#
+#   Module DNAPROCEDURES
+#       This module has functions inmplemented to make procedures
+#       on dna sequence to perform sequence preprosessing to carry
+#       out dna comparisions and identifying unique sequences in genome.
+#
+#   Author:     Javier Montalvo-Arredondo.
---
+++ scripts/dnastats.py
@@ -0,0 +1,25 @@
+###########################################################
+#
+#   Module DNASTATS
+#       Functions used to calculate sequence statistics
+#
+#   Author:     Javier Montalvo-Arredondo
+#   Contact:    Biotechnology division at,
+#               Universidad Autonoma Agraria Antonio Narro,
+#               Calzada Antonio Narro,
+#               San Buenavista, Saltillo, Coahuila, México,
+#               buitrejma[at]gmail.com
+#
+#############################################################
+#
+#
+#
+#
+
+def au_gc_ratio():
+    """
+
+    """
+    pass
+
+
