da54c73672e391f0a94ea4413c368fdff41fa9fd
Exseivier Nope <exseivier@users.noreply.github.com>
2019-02-01 11:56:41 -0600

    Update select-best-useqs.py

--- scripts/select-best-useqs.py
+++ scripts/select-best-useqs.py
@@ -9,5 +9,4 @@ mfe = dp.loadMFE(file_input)
 FHOUT = open(file_output, "w+")
 for key, value in sorted(mfe.iteritems(), key=lambda (k,v): (v["nMFE"],k), reverse=True):
     FHOUT.write("%s\t%s\n" % (key, value))
-
-
+FHOUT.close()
